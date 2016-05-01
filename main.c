a88fff1be30e2fd681bb02e8a62be6ddeb85343d
ZerAtaii <antoine.chone@telecomnancy.net>
2016-05-01 15:20:44 +0200

    Added files via upload

--- main.c
+++ main.c
@@ -9,29 +9,18 @@
 int main(int argc,char *argv[]) {
 	bankAccount hisAcc;
 	initialize(hisAcc);
-	changeAmount(&hisAcc,10000);
-	//readF(hisAcc,"DT.txt");	
+	readF(&hisAcc,"DT.txt");	
 	getInfo(&hisAcc);
 	//getTotalSpendings(hisAcc);
 	//printf("%f \n",getTotalSpendings(hisAcc));
 	//getTotalSpendingType(hisAcc);
 	//getStatsSpendings(hisAcc);
-	movements m;
-	setInfoMovements(&m,-25,"Ikea",DWELLING,NONE,"25/12/2015");
-
-	movements w;
-	movements z;
-	setInfoMovements(&z,-60,"Decathlon",OTHERSPENDING,NONE,"12/02/2016");
-	setInfoMovements(&w,55,"Merci Papa",NONES,PRESENT,"25/03/2016");
-
-
-	addMovements(&hisAcc,&m);
-	addMovements(&hisAcc,&w);
-	addMovements(&hisAcc,&z);
+	getMovements(&hisAcc);
 	printf("%f \n",getTotalSpendings(hisAcc));
 	int i;
 	for (i=0; i<7; i++) {
-		printf("%f \n",getTotalSpendingType(hisAcc)[i]);
-		printf("%f \n",getStatsSpendings(hisAcc)[i]);
+		printSpending(i);
+		printf(": %f€ qui représente ",getTotalSpendingType(hisAcc)[i]);
+		printf(" %f pourcents des dépenses.\n",getStatsSpendings(hisAcc)[i]);
 	}
 }
--- readF.c
+++ readF.c
@@ -8,33 +8,82 @@
 
 
 
-void readF(bankAccount acc,char* fichierR) {
+void readF(bankAccount *acc,char* fichierR) {
 	FILE* fichier = NULL;
 	char* c = malloc(1);
 	fichier = fopen(fichierR,"r");
-	char *string1,*string2;
+	char *string1,*string2,*i,*n;
 	if (fichier == NULL) {
 		printf("Oops ! \n");
 	}
 	else {	
 		string1=malloc(50);
 		string2=malloc(50);
-		while(c[0] != 0x20) {
+		i=malloc(50);
+		n=malloc(3);
+		while(c[0] != 0x20) {    //On recup le nom
 			fgets(c, 2, fichier);
 			strcat(string1,c);			
 		}
-		strcat(string2,fgets(c, 2, fichier));
-		while(c[0] != 0x20) {
+		strcat(string2,fgets(c, 2, fichier)); //Le premier caractère apres l'espace
+		while(c[0] != 0x20) {  // On recup le prénom
 			fgets(c,2,fichier);
 			strcat(string2,c);
 		}
-	setName(&acc,string1,string2);
+		while(c[0] != 0x3a) {	//On saute les caractères jusqu'au :
+			fgets(c,2,fichier);
+		}
+		strcat(i,fgets(c, 2, fichier));
+		while(c[0] != 0x20) { //On recupère le string qui représentera un double;
+			fgets(c,2,fichier);
+			strcat(i,c);
+		}
+		while(c[0] != 0x3a) { //On saute les caractères jusqu'au :
+			fgets(c,2,fichier);
+		}
+		while(c[0] != 0x20) { //On récupère le string qui représentera le nombre de movements
+			fgets(c,2,fichier);
+			strcat(n,c);
+		}
+		int nombre=atoi(n);
+		fseek(fichier,70, SEEK_CUR);
+		int compteur=0;
+		//for (compteur=0;compteur<nombre;compteur++) {
+			movements m;
+			char *date=malloc(15);
+			char *whereSpent=malloc(100);
+			char *amountSpent=malloc(20);
+			while(c[0] != 0x20) {
+				fgets(c,2,fichier);
+				strcat(date,c);
+			}
+			while(c[0] == 0x20) {
+				fgets(c,2,fichier);
+			}
+			strcat(amountSpent,c);
+			while(c[0] != 0x20) {
+				fgets(c,2,fichier);
+				strcat(amountSpent,c);
+			}
+			while(c[0] == 0x20) {
+				fgets(c,2,fichier);
+			}
+			strcat(whereSpent,c);
+			while(c[0] != 0x20) {
+				fgets(c,2,fichier);
+				strcat(whereSpent,c);
+			}
+			printf("%s %s %s \n",date,amountSpent,whereSpent);
+			setInfoMovements(&m,atoi(amountSpent),whereSpent,10,10,date);
+			addMovements(acc,&m);
+			
+	//	}
 
 
-
-
-        fclose(fichier); 
-    }
+        	fclose(fichier); 
+    	}
+	setName(acc,string1,string2);
+	changeAmount(acc,atof(i));	
 	
 	
-}
+}	
