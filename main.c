4421cfd53caa8045bcb1e876f0c9269cbd31a9e9
ZerAtaii <antoine.chone@telecomnancy.net>
2016-05-03 18:46:03 +0200

    Added files via upload

--- main.c
+++ main.c
@@ -11,14 +11,10 @@ int main(int argc,char *argv[]) {
 	initialize(hisAcc);
 	readF(&hisAcc,"DT.txt");	
 	getInfo(&hisAcc);
-	//getTotalSpendings(hisAcc);
-	//printf("%f \n",getTotalSpendings(hisAcc));
-	//getTotalSpendingType(hisAcc);
-	//getStatsSpendings(hisAcc);
 	getMovements(&hisAcc);
 	printf("%f \n",getTotalSpendings(hisAcc));
 	int i;
-	for (i=0; i<7; i++) {
+	for (i=0; i<8; i++) {
 		printSpending(i);
 		printf(": %f€ qui représente ",getTotalSpendingType(hisAcc)[i]);
 		printf(" %f pourcents des dépenses.\n",getStatsSpendings(hisAcc)[i]);
